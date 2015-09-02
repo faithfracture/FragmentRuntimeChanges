c7810d5360cd3fd6646e4579ce51c75df4cbf645
Jordan Bondo <jordan.bondo@gmail.com>
2015-09-02 15:57:37 -0700

    Finished adding lifecycle methods and saving fragment instance state

--- fragmentRuntimeChanges/src/main/java/com/example/fragmentruntimechanges/fragmentruntimechanges/MainActivity.java
+++ fragmentRuntimeChanges/src/main/java/com/example/fragmentruntimechanges/fragmentruntimechanges/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fragmentruntimechanges.fragmentruntimechanges;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,6 +10,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Activity", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
@@ -18,17 +20,43 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    protected void onStart() {
+        Log.d("Activity", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("Activity", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("Activity", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("Activity", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Activity", "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
--- fragmentRuntimeChanges/src/main/java/com/example/fragmentruntimechanges/fragmentruntimechanges/MainFragment.java
+++ fragmentRuntimeChanges/src/main/java/com/example/fragmentruntimechanges/fragmentruntimechanges/MainFragment.java
@@ -1,29 +1,133 @@
 package com.example.fragmentruntimechanges.fragmentruntimechanges;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
+    private static final String FRAG_1 = "Fragment 1";
+    private static final String FRAG_2 = "Fragment 2";
+
+    private static final String TAG_KEY = "TAG";
+    private String TAG = FRAG_1;
+
+    private Button button;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d("Fragment", "onCreate");
+    public void onAttach(Context context) {
+        Log.d(TAG, "onAttach");
+        super.onAttach(context);
     }
 
     @Override
-    public void onDestroy() {
-        Log.d("Fragment", "onDestroy");
-        super.onDestroy();
+    public void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            String savedTag = savedInstanceState.getString(TAG_KEY);
+            if (savedTag != null) {
+                TAG = savedTag;
+            }
+        }
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+        button = (Button) view.findViewById(R.id.hello_button);
+        button.setOnClickListener(this);
+        button.setText(TAG);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onViewStateRestored(Bundle savedInstanceState) {
+        Log.d(TAG, "onViewStateRestored");
+        super.onViewStateRestored(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+        outState.putString(TAG_KEY, TAG);
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d(TAG, "onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach");
+        super.onDestroy();
+        super.onDetach();
+    }
+
+    // region View.OnClickListener
+    @Override
+    public void onClick(View v) {
+        if (v == button) {
+            if (TAG.equals(FRAG_1)) {
+                MainFragment frag2 = new MainFragment();
+                frag2.TAG = FRAG_2;
+                getActivity().getFragmentManager().beginTransaction().replace(R.id.container, frag2,
+                        FRAG_2).addToBackStack(null).commit();
+            } else if (TAG.equals(FRAG_2)) {
+                getActivity().getFragmentManager().popBackStack();
+            }
+        }
+    }
+    // endregion
 }
